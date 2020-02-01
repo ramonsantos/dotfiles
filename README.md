c8c3917cc36525d48403146b31a12a79c16e3833
ramonsantos <ramonsantos.pe@gmail.com>
2020-02-01 15:22:01 -0300

    Add setup Fedora script step 2

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dotfiles
+
+## Setup Fedora and Install Apps
+
+Run step 1:
+
+```bash
+chmod +x setup_fedora_1.sh
+sudo -E ./setup_fedora_1.sh
+```
+
+Reboot the system and run step 2:
+
+```bash
+chmod +x setup_fedora_2.sh
+sudo -E ./setup_fedora_2.sh
+```
---
+++ setup_fedora_2.sh
@@ -0,0 +1,24 @@
+function install_app_by_snap() {
+  sudo snap install postman
+  sudo snap install heroku --classic
+  sudo snap install sublime-text --classic
+  sudo snap install crystal --classic
+}
+
+function install_node_packages() {
+  sudo npm install --global webpack gulp hexo
+}
+
+function install_python_packages() {
+  /usr/bin/python3 -m pip install -U autopep8 --user
+}
+
+function config_git() {
+  cp .gitconfig $HOME
+}
+
+# Main
+install_app_by_snap
+install_node_packages
+install_python_packages
+config_git
