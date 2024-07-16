857640a7a779c5fea9c0619c41b7c189ad837a95
ramonsantos <ramonsantos.pe@gmail.com>
2024-07-15 22:20:36 -0300

    Update docker instalation

--- config_docker.sh
+++
@@ -1,7 +0,0 @@
-echo -e "\e[1;35m Configuring Docker... \e[0m\n"
-
-sudo systemctl start --now docker
-sudo systemctl enable --now docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo systemctl restart docker
--- install_docker.sh
+++ install_docker.sh
@@ -11,7 +11,3 @@ sudo systemctl enable --now docker
 sudo usermod -aG docker ramonsantos
 newgrp docker
 sudo systemctl restart docker
-
-# Docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
